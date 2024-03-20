a305b6eedb181fccb714897105a45430c06c9bd8
QuocDut <21521842@gm.uit.edu.vn>
2024-03-20 15:27:14 +0700

    fix bai2

--- BTTH1_21521842/bai2.java
+++ BTTH1_21521842/bai2.java
@@ -1,5 +1,17 @@
 package BTTH1_21521842;
 
 public class bai2 {
+    //
+    public static double calculateS(int n, double x) {
+        double sum = 0;
+        int denominator = 0;
+
+        for (int i = 1; i <= n; i++) {
+            denominator += i;
+            sum += Math.pow(x, i) / denominator;
+        }
+
+        return sum;
+    }
 
 }
